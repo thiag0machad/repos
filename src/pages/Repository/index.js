ba39f76bd1fc2185487207a0c85e115199b85e45
Thiago Machado <thcosta0@hotmail.com>
2022-01-07 16:08:07 -0300

    class 16

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,3 @@
+export default function Repository() {
+  return <h1>Repositorio</h1>;
+}
---
+++ src/router.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from './pages/Main';
+import Repository from './pages/Repository';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/repository/:repository' element={<Repository />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
