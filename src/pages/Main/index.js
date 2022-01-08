48d538fc711e844f8739c8b3f97a1e8ad2714d63
Thiago Machado <thcosta0@hotmail.com>
2022-01-08 11:28:47 -0300

    class 24

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,7 @@ import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 import { useCallback, useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
 
 export default function Main() {
   const [newRepo, setNewRepo] = useState('');
@@ -105,9 +106,9 @@ export default function Main() {
               </DeleteButton>
               {repo.name}
             </span>
-            <a href=''>
+            <Link to={`/repository/${encodeURIComponent(repo.name)}`}>
               <FaBars size={20} />
-            </a>
+            </Link>
           </li>
         ))}
       </List>
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,3 +1,34 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../../services/api';
+import { Container } from './styles';
+
 export default function Repository() {
-  return <h1>Repositorio</h1>;
+  const [repo, setRepo] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const { repository } = useParams();
+
+  useEffect(() => {
+    async function load() {
+      const [repositoryData, issuesData] = await Promise.all([
+        api.get(`/repos/${repository}`),
+        api.get(`/repos/${repository}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ]);
+
+      setRepo(repositoryData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    load();
+  }, [repository]);
+
+  return <Container></Container>;
 }
